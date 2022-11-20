79376ebd4d3f16c03f1e0643dcfc71a21d8209e9
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2022-11-20 21:20:24 +0200

    Add files via upload

---
+++ flex_block/script.js
@@ -0,0 +1,88 @@
+/* for ( let i =1; i < 100; i++) {
+    console.log(i);
+}
+
+let i = 1;
+while ( i < 100 ) {
+    console.log(i);
+    i=i+1;
+}
+
+for (var i = 1; i <= 2; i++) {
+
+    for (var j = 1; j <=5; j++) {
+      console.log("Week" + i+ "day" + j)  
+    }
+}
+for (var i = 100; i > 10; i = i - 10) {
+    for (var j = 10; j > 4; j = j - 5) {
+        console.log(i + " divided by " + j + " equals " + i / j);
+    }
+}
+
+var i = 3;
+if(i < 5) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+
+  var i = 1;
+  if(i == 0 && i == 1) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+  for (i = 0; i < 2; i++) {
+    console.log("Hello");
+}
+
+
+var i = 0;
+while(i < 3) {
+  console.log("Hello");
+  i++;
+}
+ */
+
+/* for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < 3; j++) {
+        console.log("Hello");
+    }​
+} //dont work
+
+var i = 7;
+if(i <= 5) {
+    console.log("Hello");
+  } else if(i <= 10) {
+    console.log("Goodnight");
+  } else {
+    console.log("Goodbye");
+  }
+
+  var i =3;
+  if(i == 2 || i == 3) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+  for(var i = 0; i <= 5; i++) {
+    console.log("Hello");
+  }
+  var i = 3;
+  var j = 5;
+
+  if(i == 3 && j < 5) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+
+  var i = 7;
+  var j = 2;
+
+  if(i < 7 || j < 5) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  } */
